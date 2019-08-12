f7a78f51f11d7e5addb8deac22925c3a0c5103a6
Alex Osipchuk <donqustix@gmail.com>
2019-08-12 14:50:02 +0300

    enhance pixelization

--- lock_screen.sh
+++ lock_screen.sh
@@ -6,7 +6,7 @@ img="$HOME/.cache/i3lock.png"
 scrot -o $img
 
 # pixelate it
-convert -scale 25% -scale 400% $img $img
+convert -scale 10% -scale 1000% $img $img
 
 # lock the screen
 i3lock -u -i $img
